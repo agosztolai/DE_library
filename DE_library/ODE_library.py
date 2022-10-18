6c1259f8d5ff4fd1e149b61ceb1e44a23a400dad
Adam Gosztolai <45966708+agosztolai@users.noreply.github.com>
2022-10-18 11:28:14 +0200

    add noise functions

--- DE_library/ODE_library.py
+++ DE_library/ODE_library.py
@@ -9,16 +9,16 @@ Library of standard dynamical systems
 
 """
 
-def load_ODE(whichmodel, P=None):
+def load_ODE(whichmodel, par=None):
     """
     Load ODE system
 
-    Parameters
+    pararameters
     ----------
     whichmodel : sting
         ODE system from ODE_library.py.
-    P : dict, optional
-        Parameters. The default is None.
+    par : dict, optional
+        pararameters. The default is None.
 
     Returns
     -------
@@ -29,20 +29,20 @@ def load_ODE(whichmodel, P=None):
 
     """
 
-    if P == None:
+    if par == None:
         f, jac = getattr(sys.modules[__name__], "fun_%s" % whichmodel)()
     else:
-        f, jac = getattr(sys.modules[__name__], "fun_%s" % whichmodel)(P)
+        f, jac = getattr(sys.modules[__name__], "fun_%s" % whichmodel)(par)
                      
     return f, jac
 
 
-def fun_saddle_node(P = {'mu': 1}):
-    """Prototypical system exhibiting a saddle node bifurcation at mu=0"""
+def fun_saddle_node(par = {'mu': 1}):
+    """parrototypical system exhibiting a saddle node bifurcation at mu=0"""
     
     def f(t, X):
         x, y = X
-        f1 = P['mu'] - x**2
+        f1 = par['mu'] - x**2
         f2 = -y
         
         return [f1, f2]
@@ -57,19 +57,19 @@ def fun_saddle_node(P = {'mu': 1}):
     return f, jac
     
 
-def fun_trans_pitch(P = {'mu': 1}):
-    """Prototypical system exhibiting a *transcritical* pitchfork bifurcation at mu=0"""
+def fun_trans_pitch(par = {'mu': 1}):
+    """parrototypical system exhibiting a *transcritical* pitchfork bifurcation at mu=0"""
     
     def f(t, X):
         x, y = X
-        f1 = P['mu']*x - x**2
+        f1 = par['mu']*x - x**2
         f2 = -y
         
         return [f1, f2]
     
     def jac(t, X):
         x, y = X
-        dfdx = [P['mu']-2*x, 0.0]
+        dfdx = [par['mu']-2*x, 0.0]
         dfdy = [0.0, -1.0]
         
         return [dfdx, dfdy]
@@ -77,19 +77,19 @@ def fun_trans_pitch(P = {'mu': 1}):
     return f, jac
     
     
-def fun_sup_pitch(P = {'mu': 1}):
-    """Prototypical system exhibiting a *supercritical* pitchfork bifurcation at mu=0"""
+def fun_sup_pitch(par = {'mu': 1}):
+    """parrototypical system exhibiting a *supercritical* pitchfork bifurcation at mu=0"""
     
     def f(t, X):
         x, y = X
-        f1 = P['mu']*x - x**3
+        f1 = par['mu']*x - x**3
         f2 = -y
         
         return [f1, f2]
     
     def jac(t, X):
         x, y = X
-        dfdx = [P['mu']-3*x**2, 0.0]
+        dfdx = [par['mu']-3*x**2, 0.0]
         dfdy = [0.0, -1.0]
         
         return [dfdx, dfdy]
@@ -97,19 +97,19 @@ def fun_sup_pitch(P = {'mu': 1}):
     return f, jac
     
     
-def fun_sub_pitch(P = {'mu': 1}):
-    """Prototypical system exhibiting a *subcritical* pitchfork bifurcation at mu=0"""
+def fun_sub_pitch(par = {'mu': 1}):
+    """parrototypical system exhibiting a *subcritical* pitchfork bifurcation at mu=0"""
     
     def f(t, X):
         x, y = X
-        f1 = P['mu']*x + x**3
+        f1 = par['mu']*x + x**3
         f2 = -y
         
         return [f1, f2]
     
     def jac(t, X):
         x, y = X
-        dfdx = [P['mu']+3*x**2, 0.0]
+        dfdx = [par['mu']+3*x**2, 0.0]
         dfdy = [0.0, -1.0]
         
         return [dfdx, dfdy]
@@ -117,103 +117,103 @@ def fun_sub_pitch(P = {'mu': 1}):
     return f, jac
     
     
-def fun_sup_hopf(P = {'mu': 1, 'omega': 1, 'b': 1}):
-    """Prototypical system exhibiting a *supercritical* Hopf bifurcation at mu=0"""
+def fun_sup_hopf(par = {'mu': 1, 'omega': 1, 'b': 1}):
+    """parrototypical system exhibiting a *supercritical* Hopf bifurcation at mu=0"""
     
     def f(t, X):
         x, y = X
-        f1 = P['mu']*x - x**3
-        f2 = P['omega'] + P['b']*x**2
+        f1 = par['mu']*x - x**3
+        f2 = par['omega'] + par['b']*x**2
         
         return [f1, f2]
     
     def jac(t, X):
         x, y = X
-        dfdx = [P['mu']-3*x**2, 0.0]
-        dfdy = [2*P['b']*x, 0.0]
+        dfdx = [par['mu']-3*x**2, 0.0]
+        dfdy = [2*par['b']*x, 0.0]
         
         return [dfdx, dfdy]
     
     return f, jac
     
     
-def fun_sub_hopf(P = {'mu': 1, 'omega': 1, 'b': 1}):
-    """Prototypical system exhibiting a *subcritical* Hopf bifurcation at mu=0"""
+def fun_sub_hopf(par = {'mu': 1, 'omega': 1, 'b': 1}):
+    """parrototypical system exhibiting a *subcritical* Hopf bifurcation at mu=0"""
     
     def f(t, X):
         x, y = X
-        f1 = P['mu']*x + x**3 - x**5
-        f2 = P['omega'] + P['b']*x**2
+        f1 = par['mu']*x + x**3 - x**5
+        f2 = par['omega'] + par['b']*x**2
         
         return [f1, f2]
     
     def jac(t, X):
         x, y = X
-        dfdx = [P['mu']+3*x**2-5*x**4, 0.0]
-        dfdy = [2*P['b']*x, 0.0]
+        dfdx = [par['mu']+3*x**2-5*x**4, 0.0]
+        dfdy = [2*par['b']*x, 0.0]
         
         return [dfdx, dfdy]
     
     return f, jac
         
 
-def fun_double_pendulum(P = {'b': 0.05, 'g': 9.81, 'l': 1.0, 'm': 1.0}):
+def fun_double_pendulum(par = {'b': 0.05, 'g': 9.81, 'l': 1.0, 'm': 1.0}):
     """Double pendulum"""
     
     def f(t, X):
         x, y = X
         f1 = y
-        f2 = -(P['b']/P['m'])*y - P['g']*np.sin(x)
+        f2 = -(par['b']/par['m'])*y - par['g']*np.sin(x)
         
         return [f1, f2]
     
     def jac(t, X):
         x, y = X
         dfdx = [0.0, 1.0]
-        dfdy = [-P['g']*np.cos(x), -P['b']/P['m']]
+        dfdy = [-par['g']*np.cos(x), -par['b']/par['m']]
               
         return [dfdx, dfdy]
     
     return f, jac
 
 
-def fun_lorenz(P = {'sigma': 10.0, 'beta': 8/3.0, 'rho': 28.0, 'tau': 1.0}):
+def fun_lorenz(par = {'sigma': 10.0, 'beta': 8/3.0, 'rho': 28.0, 'tau': 1.0}):
     """Lorenz system"""
     
     def f(t, X):
         x, y, z = X
-        f1 = P['sigma']*(y - x)/P['tau']
-        f2 = (x*(P['rho'] - z) - y)/P['tau']
-        f3 = (x*y - P['beta']*z)/P['tau']
+        f1 = par['sigma']*(y - x)/par['tau']
+        f2 = (x*(par['rho'] - z) - y)/par['tau']
+        f3 = (x*y - par['beta']*z)/par['tau']
         
         return [f1, f2, f3]
     
     def jac(t, X):
         x, y, z = X
-        dfdx = [-P['sigma']/P['tau'], P['sigma']/P['tau'], 0.]
-        dfdy = [(P['rho'] - z)/P['tau'], -1./P['tau'], -x/P['tau']]
-        dfdz = [y/P['tau'], x/P['tau'], -P['beta']/P['tau']]
+        dfdx = [-par['sigma']/par['tau'], par['sigma']/par['tau'], 0.]
+        dfdy = [(par['rho'] - z)/par['tau'], -1./par['tau'], -x/par['tau']]
+        dfdz = [y/par['tau'], x/par['tau'], -par['beta']/par['tau']]
         
         return [dfdx, dfdy, dfdz]
                 
     return f, jac            
     
 
-def fun_rossler(P = {'a': 0.15, 'b': 0.2, 'c': 10.0}):
+def fun_rossler(par = {'a': 0.15, 'b': 0.2, 'c': 10.0}):
     """Rossler system"""
     
     def f(t, X):
         x, y, z = X
         f1 = -y - z
-        f2 = x + P['a']*y
-        f3 = P['b'] + z * (x - P['c'])
+        f2 = x + par['a']*y
+        f3 = par['b'] + z * (x - par['c'])
         
         return [f1, f2, f3]
     
     def jac(t, X):
         x, y, z = X
         dfdx = [0.,      -1, -1 ]
-        dfdy = [1,   P['a'],  0.]
+        dfdy = [1,   par['a'],  0.]
         dfdz = [z,       0.,  x ]
         
         return [dfdx, dfdy, dfdz]
@@ -221,41 +221,43 @@ def fun_rossler(P = {'a': 0.15, 'b': 0.2, 'c': 10.0}):
     return f, jac
 
 
-def fun_vanderpol(P = {'mu': 1.}):
-    """Van der Pol oscillator"""
+def fun_vanderpol(par = {'mu': 1.}):
+    """Van der parol oscillator"""
     
     def f(t, X):
         x, y = X
         f1 = y
-        f2 = P['mu']*(1-x**2)*y - x
+        f2 = par['mu']*(1-x**2)*y - x
         
         return [f1, f2]
     
     def jac(t, X):
         x, y = X
-        dfdx = [0.,                1.    ]
-        dfdy = [-2.*P['mu']*x*y - 1., -P['mu']*x**2]
+        dfdx = [0.,                     1.             ]
+        dfdy = [-2.*par['mu']*x*y - 1., -par['mu']*x**2]
         
         return [dfdx, dfdy]
 
     return f, jac
 
 
-def fun_duffing(P = {'alpha', 'beta', 'gamma', 'delta', 'omega', 'tau'}):
+def fun_duffing(par = {'alpha': 1., 'beta': 1., 'gamma': .1, 'delta': 2., 'omega': 1., 'tau': 1.}):
     """Duffing oscillator"""
     
     def f(t, X):
         x, y, z = X
-        f1 = y/P['tau']
-        f2 = (-P['delta']*y - P['alpha']*x - P['beta']*x**3 + P['gamma']*np.cos(z))/P['tau']
-        f3 = P['omega']
+        f1 = y/par['tau']
+        f2 = (-par['delta']*y - par['alpha']*x - par['beta']*x**3 + par['gamma']*np.cos(z))/par['tau']
+        f3 = par['omega']
         
         return [f1, f2, f3]
     
     def jac(t, X):
         x, y, z = X
-        dfdx = [0., 1./P['tau'], 0.]
-        dfdy = [(-P['alpha'] - 3*P['beta']*x**2)/P['tau'], -P['delta']/P['tau'], -P['gamma']*np.sin(z)/P['tau']]
+        dfdx = [0., 1./par['tau'], 0.]
+        dfdy = [(-par['alpha'] - 3*par['beta']*x**2)/par['tau'], 
+                -par['delta']/par['tau'], 
+                -par['gamma']*np.sin(z)/par['tau']]
         dfdz = [0., 0., 0.]
 
         return [dfdx, dfdy, dfdz]
@@ -263,7 +265,28 @@ def fun_duffing(P = {'alpha', 'beta', 'gamma', 'delta', 'omega', 'tau'}):
     return f, jac
 
 
-def fun_kuramoto(P = {'W': np.array([28, 19, 11, 9, 2, 4]), 
+def fun_bogdanov_takens(par = {'beta1':-0.1, 'beta2':0}):
+    """Bogdanov-Takens oscillator"""
+    
+    def f(t, X):
+        x, y = X
+        f1 = y
+        f2 = par['beta1'] + par['beta2']*x + x**2 - x*y
+        
+        return [f1, f2]
+    
+    def jac(t, X):
+        x, y = X
+        dfdx = [1., 0.]
+        dfdy = [par['beta2'] + 2*x - y, 
+                -x]
+
+        return [dfdx, dfdy]
+
+    return f, jac
+
+
+def fun_kuramoto(par = {'W': np.array([28, 19, 11, 9, 2, 4]), 
                       'K': np.array([[ 0,   -0.5, -0.5, -0.5,  1,   -0.5],
                                      [-0.5,  0,   -0.5, -0.5, -0.5,  1  ],
                                      [-0.5, -0.5,  0,    1,   -0.5, -0.5],
@@ -279,26 +302,26 @@ def fun_kuramoto(P = {'W': np.array([28, 19, 11, 9, 2, 4]),
 # #            if self.noise != None:
 # #                n = self.noise().astype(self.dtype)
 # #                phase += n
-#         phase = P['W'] + np.sum(P['K']*np.sin(dX), axis=0)
+#         phase = par['W'] + np.sum(par['K']*np.sin(dX), axis=0)
 
 #         return phase
     
     def f(t, X):
         Xt = X[:, None]
         dX = X-Xt
-        phase = P['W'].astype(float)
+        phase = par['W'].astype(float)
         # if self.noise != None:
         #     n = self.noise().astype(self.dtype)
         #     phase += n
-        phase += np.sum(P['K']*np.sin(dX),axis=1)
+        phase += np.sum(par['K']*np.sin(dX),axis=1)
 
         return phase
 
     # def jac(t, X):
     #     Xt = X[:, None]
     #     dX = X - Xt
-    #     phase = np.zeros(P['K'].shape)
-    #     tmp = P['K']*np.cos(dX)
+    #     phase = np.zeros(par['K'].shape)
+    #     tmp = par['K']*np.cos(dX)
     #     tmp -= np.diag(tmp)
     #     phase += np.diag(np.sum(tmp, axis=0))
     #     phase -= tmp
@@ -310,13 +333,13 @@ def fun_kuramoto(P = {'W': np.array([28, 19, 11, 9, 2, 4]),
         Xt = X[:,None]
         dX = X-Xt
         
-        # m_order = P['K'].shape[0]
+        # m_order = par['K'].shape[0]
 
-        # phase = [m*P['K'][m-1]*np.cos(m*dX) for m in range(1,1+m_order)]
-        phase = P['K']*np.cos(dX)
+        # phase = [m*par['K'][m-1]*np.cos(m*dX) for m in range(1,1+m_order)]
+        phase = par['K']*np.cos(dX)
         phase = np.sum(phase, axis=0)
 
-        for i in range(P['K'].shape[0]):
+        for i in range(par['K'].shape[0]):
             phase[i,i] = -np.sum(phase[:,i])
 
         return phase
@@ -324,7 +347,7 @@ def fun_kuramoto(P = {'W': np.array([28, 19, 11, 9, 2, 4]),
     return f, jac    
 
 
-def fun_kuramoto_delay(P = {
+def fun_kuramoto_delay(par = {
                     'W': np.array([28, 19, 11, 9, 2, 4]), 
                     'K': np.array([[ 0,   -0.5, -0.5, -0.5,  1,   -0.5],
                                      [-0.5,  0,   -0.5, -0.5, -0.5,  1  ],
@@ -342,39 +365,39 @@ def fun_kuramoto_delay(P = {
 #            if self.noise != None:
 #                n = self.noise().astype(self.dtype)
 #                phase += n
-        phase = P['W'] + np.sum(P['K']*np.sin(dX), axis=0)
+        phase = par['W'] + np.sum(par['K']*np.sin(dX), axis=0)
 
         return phase
 
     return f
  
     
-def fun_righetti_ijspeert(P = {'a', 'alpha', 'mu', 'K', 'omega_swing', 'omega_stance'}):
+def fun_righetti_ijspeert(par = {'a', 'alpha', 'mu', 'K', 'omega_swing', 'omega_stance'}):
     """Righetti-Ijspeert phase oscillator"""
 
     def f(t, X):
         x = np.array(X[:6])
         y = np.array(X[6:])
             
-        omega = P['omega_stance'] + (P['omega_swing'] - P['omega_stance']) / (1+np.exp(P['a']*y))            
-        R = P['alpha']*(P['mu'] - x**2 - y**2)
+        omega = par['omega_stance'] + (par['omega_swing'] - par['omega_stance']) / (1+np.exp(par['a']*y))            
+        R = par['alpha']*(par['mu'] - x**2 - y**2)
             
-        return (R*x - omega*y).tolist() + (R*y + omega*x + P['K'].dot(y)).tolist()
+        return (R*x - omega*y).tolist() + (R*y + omega*x + par['K'].dot(y)).tolist()
 
     def jac(t, X):
         x = np.array(X[:6])
         y = np.array(X[6:])
             
-        omega = P['omega_stance'] + (P['omega_swing'] - P['omega_stance']) / (1+np.exp(P['a']*y))            
-        R = P['alpha']*(P['mu'] - x**2 - y**2)
+        omega = par['omega_stance'] + (par['omega_swing'] - par['omega_stance']) / (1+np.exp(par['a']*y))            
+        R = par['alpha']*(par['mu'] - x**2 - y**2)
         
         dX = np.zeros([12,12])
-        dX[6:11,6:11] = P['K']
+        dX[6:11,6:11] = par['K']
         for i in range(6):
-            dX[i,i]     = - 2*P['alpha']*x[i]**2 + R[i] 
-            dX[i,i+6]   = - 2*P['alpha']*x[i]*y[i] - omega[i] + (omega[i]-P['omega_stance'])**2*P['a']*np.exp(P['a']*y[i])/(P['omega_swing']-P['omega_stance'])*y[i] 
-            dX[i+6,i]   = - 2*P['alpha']*x[i]*y[i] + omega[i]
-            dX[i+6,i+6] = - 2*P['alpha']*y[i]*2 + R[i] - (omega[i]-P['omega_stance'])**2*P['a']*np.exp(P['a']*y[i])/(P['omega_swing']-P['omega_stance'])*x[i]
+            dX[i,i]     = - 2*par['alpha']*x[i]**2 + R[i] 
+            dX[i,i+6]   = - 2*par['alpha']*x[i]*y[i] - omega[i] + (omega[i]-par['omega_stance'])**2*par['a']*np.exp(par['a']*y[i])/(par['omega_swing']-par['omega_stance'])*y[i] 
+            dX[i+6,i]   = - 2*par['alpha']*x[i]*y[i] + omega[i]
+            dX[i+6,i+6] = - 2*par['alpha']*y[i]*2 + R[i] - (omega[i]-par['omega_stance'])**2*par['a']*np.exp(par['a']*y[i])/(par['omega_swing']-par['omega_stance'])*x[i]
             
         return dX.tolist()
 
--- DE_library/solvers.py
+++ DE_library/solvers.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import numpy.ma as ma
 from scipy.integrate import odeint
 from DE_library import ODE_library
 
-def simulate_ODE(whichmodel, t, X0, P=None):
+def simulate_ODE(whichmodel, t, X0, par=None, **noise_pars):
     """
     Load ODE functions and run appropriate solver
 
@@ -29,12 +30,182 @@ def simulate_ODE(whichmodel, t, X0, P=None):
 
     """
     
-    f, jac = ODE_library.load_ODE(whichmodel, P=None)
+    f, jac = ODE_library.load_ODE(whichmodel, par=None)
     X = solve_ODE(f, jac, t, X0)
+    
+    if noise_pars!={}:
+        X = addnoise(X, **noise_pars)
     
     Xprime = derivative(f, t, X)
     
     return X, Xprime
+
+
+def addnoise(X, **noise_pars):
+    """
+    Add noise to trajectories
+
+    Parameters
+    ----------
+    X : np array
+        Trajectories.
+    **noise_pars : additional keyword argument to specify noise parameters
+
+    Returns
+    -------
+    X : len(t)xlen(X0) numpy array
+        Trajectory.
+
+    """
+    
+    if noise_pars['noise']=='Gaussian':
+        mu = noise_pars['mu']
+        sigma = noise_pars['sigma']
+        X += np.random.normal(mu, sigma, size = X.shape)
+        
+    return X
+
+
+def generate_trajectories(whichmodel, n, t, X0_range, par=None, stack=True, transient=None, seed=None, **noise_pars):
+    """
+    Generate an ensemble of trajectories from different initial conditions, 
+    chosen randomly from a box.
+
+    Parameters
+    ----------
+    whichmodel : string
+        ODE system from ODE_library.py.
+    n : int
+        Number of trajectories to generate.
+    t : array or list
+        Time steps to evaluate system at..
+    X0_range : list[list] e.g. [[0,1],[0,1],[0,1]]
+        Lower/upper limits in each dimension of the box of initial solutions.
+    par : dict, optional
+        Parameters. The default is None.
+    stack : bool, optional
+        Stack trajectories into a long array. The default is True.
+    transient : float between 0 and 1
+        Ignore first transient fraction of time series.
+    seed : int, optional
+        Seed of random initial solutions. The default is None.
+    noise : bool, optional
+        Add noise to trajectories. Default is False.
+    **noise_pars : additional keyword argument to specify noise parameters 
+
+    Returns
+    -------
+    X_ens : numpy array
+        Trajectories.
+    t_ens : numpy array
+        Time indices
+
+    """
+    
+    if seed is not None:
+        np.random.seed(seed)
+        
+    t_ens, X_ens = [], []
+    for i in range(n):
+        X0 = []
+        for r in X0_range:
+            X0.append(np.random.uniform(low=r[0], high=r[1]))
+            
+        X_ens.append(simulate_ODE(whichmodel, t, X0, par=par, **noise_pars))
+        t_ens.append(np.arange(len(t)))
+        
+        if transient is not None:
+            l_tr = int(len(X_ens[-1])*transient)
+            X_ens[-1] = X_ens[-1][l_tr:]
+            t_ens[-1] = t_ens[-1][:-l_tr]
+        
+    if stack:
+        X_ens = np.vstack(X_ens)
+        t_ens = np.hstack(t_ens)
+        
+    return t_ens, X_ens
+
+
+def sample_trajectories(X, n, T, t0=0.1, seed=None):
+    """
+    Randomly sample trajectories from the attractor.
+
+    Parameters
+    ----------
+    X : numpy array
+        Trajectory including transient.
+    n : int
+        Number of trajectories to sample.
+    T : int
+        Length of trajectories (timesteps).
+    t0 : float
+        Initial transient fraction of the time series. The default is 0.1 (10%).
+    seed : int, optional
+        Seed of random initial solutions. The default is None.
+
+    Returns
+    -------
+    t_sample : list(list)
+        Time indices in the original attrator
+    X_sample : list[array]
+        n sampled trajectories.
+
+    """
+    
+    if seed is not None:
+        np.random.seed(seed)
+    
+    #Discard transient
+    ind = np.arange(X.shape[0])
+    ind = ind[int(t0*len(ind)):len(ind)-T]
+    ts = np.random.choice(ind, size=n, replace=True)
+    
+    X_sample = generate_flow(X, ts, T=T)
+    
+    t_sample = []
+    for i in range(n):
+        t_sample+=list(np.arange(0,T))
+        
+    return t_sample, X_sample
+
+
+def generate_flow(X, ts, T):
+    """
+    Obtain trajectories of between timepoints.
+
+    Parameters
+    ----------
+    X : np array
+        Trajectories.
+    ts : int or np array or list[int]
+        Source timepoint.
+    T : int or list[int]
+        End of trajectory or time horizon.
+
+    Returns
+    -------
+    X_sample : list[np array].
+        Set of flows of length T.
+
+    """
+    
+    ts = ma.array(ts, dtype=int)
+    
+    if isinstance(T, int):
+        tt = ma.array([ts[i]+T for i in range(len(ts))])
+        tt = ma.array(tt, mask=ts.mask, dtype=int)
+    else:
+        tt = ma.array(T)
+        assert len(tt)==len(ts), 'Number of source points must equal to the \
+            number of target points.'
+    
+    X_sample = []
+    for s,t in zip(ts,tt):
+        if not ma.is_masked(s) and not ma.is_masked(t):
+            X_sample.append(X[s:t+1])
+
+    return X_sample, ts[~ts.mask], tt[~tt.mask]
+
 
 def solve_ODE(f, jac, t, X0):
     
