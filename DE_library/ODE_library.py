681e938ad72a67a7b9cfe51bc816e266f03e944f
Jiri Lhotka <jl887@cantab.ac.uk>
2022-11-15 12:12:07 +0100

    Autosave everything to let linter fix spacing issues

--- DE_library/ODE_library.py
+++ DE_library/ODE_library.py
@@ -33,7 +33,7 @@ def load_ODE(whichmodel, par=None):
         f, jac = getattr(sys.modules[__name__], "fun_%s" % whichmodel)()
     else:
         f, jac = getattr(sys.modules[__name__], "fun_%s" % whichmodel)(par)
-                     
+
     return f, jac
 
 # =============================================================================
@@ -41,204 +41,204 @@ def load_ODE(whichmodel, par=None):
 # =============================================================================
 def fun_saddle_node(par = {'mu': 1}):
     """parrototypical system exhibiting a saddle node bifurcation at mu=0"""
-    
+
     def f(t, X):
         x, y = X
         f1 = par['mu'] - x**2
         f2 = -y
-        
+
         return [f1, f2]
-    
+
     def jac(t, X):
         x, y = X
         df1 = [-2*x, 0.0]
         df2 = [0.0, -1.0]
-        
+
         return [df1, df2]
-    
+
     return f, jac
-    
+
 
 def fun_transcritical_pitchfork(par = {'mu': 1}):
     """parrototypical system exhibiting a *transcritical* pitchfork bifurcation at mu=0"""
-    
+
     def f(t, X):
         x, y = X
         f1 = par['mu']*x - x**2
         f2 = -y
-        
+
         return [f1, f2]
-    
+
     def jac(t, X):
         x, y = X
         df1 = [par['mu']-2*x, 0.0]
         df2 = [0.0, -1.0]
-        
+
         return [df1, df2]
-    
+
     return f, jac
-    
-    
+
+
 def fun_supcritical_pitchfork(par = {'mu': 1}):
     """prototypical system exhibiting a *supercritical* pitchfork bifurcation at mu=0"""
-    
+
     def f(t, X):
         x, y = X
         f1 = par['mu']*x - x**3
         f2 = -y
-        
+
         return [f1, f2]
-    
+
     def jac(t, X):
         x, y = X
         df1 = [par['mu']-3*x**2, 0.0]
         df2 = [0.0, -1.0]
-        
+
         return [df1, df2]
-    
+
     return f, jac
-    
-    
+
+
 def fun_subcritical_pitchfork(par = {'mu': 1}):
     """prototypical system exhibiting a *subcritical* pitchfork bifurcation at mu=0"""
-    
+
     def f(t, X):
         x, y = X
         f1 = par['mu']*x + x**3
         f2 = -y
-        
+
         return [f1, f2]
-    
+
     def jac(t, X):
         x, y = X
         df1 = [par['mu']+3*x**2, 0.0]
         df2 = [0.0, -1.0]
-        
+
         return [df1, df2]
-    
+
     return f, jac
-    
-    
+
+
 # =============================================================================
 # Codimension 1 bifurcations in 2D
 # =============================================================================
 def fun_supcritical_hopf(par = {'mu': 1., 'omega': 1., 'b': 1.}):
     """System exhibiting a *supercritical* Hopf bifurcation at mu=0, while
     keeping other parameters constant"""
-    
+
     def f(t, X):
         x, y = X
         f1 = (par['mu'] - x**2 - y**2)*x - par['omega']*y
         f2 = (par['mu'] - x**2 - y**2)*y + par['omega']*x
-        
+
         return [f1, f2]
-    
+
     def jac(t, X):
         x, y = X
         df1 = [par['mu'] - 3*x**2 - y**2, -2*x*y - par['omega']]
         df2 = [-2*x*y - par['omega'], par['mu'] - x**2 - 3*y**2]
-        
+
         return [df1, df2]
-    
+
     return f, jac
-    
-    
+
+
 def fun_subcritical_hopf(par = {'mu': 1., 'omega': 1., 'b': 1.}):
     """prototypical system exhibiting a *subcritical* Hopf bifurcation at mu=0
     as mu increases"""
-    
+
     def f(t, X):
         x, _ = X
         f1 = par['mu']*x + x**3 - x**5
         f2 = par['omega'] + par['b']*x**2
-        
+
         return [f1, f2]
-    
+
     def jac(t, X):
         x, _ = X
         df1 = [par['mu']+3*x**2-5*x**4, 0.0]
         df2 = [2*par['b']*x, 0.0]
-        
+
         return [df1, df2]
-    
+
     return f, jac
 
 
 def fun_saddle_node_of_cycles(par = {'mu': -1., 'omega': 1., 'b': 1.}):
     """Saddle node bifurcation of cycles at mu=-1/4"""
-    
+
     def f(t, X):
         r, _ = X
         f1 = par['mu']*r + r**3 - r**5
         f2 = par['omega'] + par['b']*r**2
-        
+
         return [f1, f2]
-    
+
     def jac(t, X):
         r, _ = X
         df1 = [par['mu']+3*r**2-5*r**4, 0.0]
         df2 = [2*par['b']*r, 0.0]
-        
+
         return [df1, df2]
-    
+
     return f, jac
 
 
 def fun_infinite_period(par = {'mu'}):
     """System exhibiting an infinite-period bifurcation at mu=1"""
-    
+
     def f(t, X):
         r, theta = X
         f1 = r*(1-r**2)
         f2 = par['mu'] - np.sin(theta)
-        
+
         return [f1, f2]
-    
+
     def jac(t, X):
         r, theta = X
         df1 = [1-r**2 - 2*r, 0.0]
         df2 = [0.0, -np.cos(theta)]
-        
+
         return [df1, df2]
-    
+
     return f, jac
 
 
 def fun_homoclinic(par = {'mu'}):
     """System exhibiting an homoclinicbifurcation at mu=-0.8645"""
-    
+
     def f(t, X):
         x, y = X
         f1 = y
         f2 = par['mu'] + x - x**2 + x*y
-        
+
         return [f1, f2]
-    
+
     def jac(t, X):
         x, y = X
         df1 = [0.0, 1.0]
         df2 = [1.0 - 2*x + y, x]
-        
+
         return [df1, df2]
-    
+
     return f, jac
 
 
 def fun_vanderpol(par = {'mu': 1.}):
     """Van der parol oscillator exhibiting a degenerate Hopf bifurcation"""
-    
+
     def f(t, X):
         x, y = X
         f1 = y
         f2 = par['mu']*(1-x**2)*y - x
-        
+
         return [f1, f2]
-    
+
     def jac(t, X):
         x, y = X
         df1 = [0.,                     1.             ]
         df2 = [-2.*par['mu']*x*y - 1., -par['mu']*x**2]
-        
+
         return [df1, df2]
 
     return f, jac
@@ -246,20 +246,20 @@ def fun_vanderpol(par = {'mu': 1.}):
 
 def fun_duffing(par = {'alpha': 1., 'beta': 1., 'gamma': .1, 'delta': 2., 'omega': 1., 'tau': 1.}):
     """Duffing oscillator"""
-    
+
     def f(t, X):
         x, y, z = X
         f1 = y/par['tau']
         f2 = (-par['delta']*y - par['alpha']*x - par['beta']*x**3 + par['gamma']*np.cos(z))/par['tau']
         f3 = par['omega']
-        
+
         return [f1, f2, f3]
-    
+
     def jac(t, X):
         x, y, z = X
         df1 = [0., 1./par['tau'], 0.]
-        df2 = [(-par['alpha'] - 3*par['beta']*x**2)/par['tau'], 
-                -par['delta']/par['tau'], 
+        df2 = [(-par['alpha'] - 3*par['beta']*x**2)/par['tau'],
+                -par['delta']/par['tau'],
                 -par['gamma']*np.sin(z)/par['tau']]
         df3 = [0., 0., 0.]
 
@@ -270,14 +270,14 @@ def fun_duffing(par = {'alpha': 1., 'beta': 1., 'gamma': .1, 'delta': 2., 'omega
 
 def brusselator(par = {'a': 1.0, 'b': 1.0}):
     """Brusselator"""
-    
+
     def f(t, X):
         x, y = X
         f1 = 1 - (par['b']+1)*x - par['a']*x**2*y
         f2 = par['b']*x + par['a']*x**2*y
-        
+
         return [f1, f2]
-    
+
     def jac(t, X):
         x, y = X
         df1 = [-(par['b']+1)*x - 2*par['a']*x*y, - par['a']*x**2]
@@ -293,14 +293,14 @@ def brusselator(par = {'a': 1.0, 'b': 1.0}):
 # =============================================================================
 def fun_bogdanov_takens(par = {'beta1': -0.1, 'beta2': 0}):
     """Bogdanov-Takens system"""
-    
+
     def f(t, X):
         x, y = X
         f1 = y
         f2 = par['beta1'] + par['beta2']*x + x**2 - x*y
-        
+
         return [f1, f2]
-    
+
     def jac(t, X):
         x, y = X
         df1 = [0., 1.]
@@ -309,70 +309,70 @@ def fun_bogdanov_takens(par = {'beta1': -0.1, 'beta2': 0}):
         return [df1, df2]
 
     return f, jac
-        
+
 
 # =============================================================================
 # Chaotic systems in 3D
 # =============================================================================
 def fun_double_pendulum(par = {'b': 0.05, 'g': 9.81, 'l': 1.0, 'm': 1.0}):
     """Double pendulum"""
-    
+
     def f(t, X):
         x, y = X
         f1 = y
         f2 = -(par['b']/par['m'])*y - par['g']*np.sin(x)
-        
+
         return [f1, f2]
-    
+
     def jac(t, X):
         x, y = X
         df1 = [0.0, 1.0]
         df2 = [-par['g']*np.cos(x), -par['b']/par['m']]
-              
+
         return [df1, df2]
-    
+
     return f, jac
 
 
 def fun_lorenz(par = {'sigma': 10.0, 'beta': 8/3.0, 'rho': 28.0, 'tau': 1.0}):
     """Lorenz system"""
-    
+
     def f(t, X):
         x, y, z = X
         f1 = par['sigma']*(y - x)/par['tau']
         f2 = (x*(par['rho'] - z) - y)/par['tau']
         f3 = (x*y - par['beta']*z)/par['tau']
-        
+
         return [f1, f2, f3]
-    
+
     def jac(t, X):
         x, y, z = X
         df1 = [-par['sigma']/par['tau'], par['sigma']/par['tau'], 0.]
         df2 = [(par['rho'] - z)/par['tau'], -1./par['tau'], -x/par['tau']]
         df3 = [y/par['tau'], x/par['tau'], -par['beta']/par['tau']]
-        
+
         return [df1, df2, df3]
-                
-    return f, jac            
-    
+
+    return f, jac
+
 
 def fun_rossler(par = {'a': 0.15, 'b': 0.2, 'c': 10.0}):
     """Rossler system"""
-    
+
     def f(t, X):
         x, y, z = X
         f1 = -y - z
         f2 = x + par['a']*y
         f3 = par['b'] + z * (x - par['c'])
-        
+
         return [f1, f2, f3]
-    
+
     def jac(t, X):
         x, y, z = X
         df1 = [0.,      -1, -1 ]
         df2 = [1,   par['a'],  0.]
         df3 = [z,       0.,  x ]
-        
+
         return [df1, df2, df3]
 
     return f, jac
@@ -386,9 +386,9 @@ def fun_oscillations_on_torus(par = {'omega1','omega2','K1','K2'}):
         t1, t2 = X
         f1 = par['omega1'] + par['K1']*np.sin(t2-t1)
         f2 = par['omega2'] + par['K2']*np.sin(t1-t2)
-        
+
         return [f1, f2]
-    
+
     def jac(t, X):
         t1, t2 = X
         df1 = [-par['K1']*np.cos(t2-t1)*t1, par['K1']*np.cos(t2-t1)*t2]
@@ -399,18 +399,16 @@ def fun_oscillations_on_torus(par = {'omega1','omega2','K1','K2'}):
     return f, jac
 
 
-def fun_kuramoto(par = {'W': np.array([28, 19, 11, 9, 2, 4]), 
+def fun_kuramoto(par = {'W': np.array([28, 19, 11, 9, 2, 4]),
                         'K': np.array([[ 0,   -0.5, -0.5, -0.5,  1,   -0.5],
                                        [-0.5,  0,   -0.5, -0.5, -0.5,  1  ],
                                        [-0.5, -0.5,  0,    1,   -0.5, -0.5],
                                        [-0.5, -0.5,  1,    0,   -0.5, -0.5],
                                        [ 1,   -0.5, -0.5, -0.5,  0,   -0.5],
-                                       [-0.5,  1,   -0.5, -0.5, -0.5,  0  ]])}    
+                                       [-0.5,  1,   -0.5, -0.5, -0.5,  0  ]])}
                  ):
-    """Kuramoto oscillator 
-    d/dt(\theta_i) + \sum_j K_ij sin(\theta_j-\theta_i)
-    """
-    
+    """Kuramoto oscillator"""
+
     def f(t, X):
         Xt = X[:, None]
         dX = X-Xt
@@ -418,47 +416,47 @@ def fun_kuramoto(par = {'W': np.array([28, 19, 11, 9, 2, 4]),
         phase += (par['K']*np.sin(dX)).sum(1)
 
         return phase
-    
+
     def jac(t, X):
 
         Xt = X[:, None]
         dX = X-Xt
-        
+
         phase = par['K']*np.cos(dX)
         phase -= np.diag(phase.sum(0))
 
         return phase
 
     return f, jac
- 
-    
+
+
 def fun_righetti_ijspeert(par = {'a', 'alpha', 'mu', 'K', 'omega_swing', 'omega_stance'}):
     """Righetti-Ijspeert phase oscillator"""
 
     def f(t, X):
         x = np.array(X[:6])
         y = np.array(X[6:])
-            
-        omega = par['omega_stance'] + (par['omega_swing'] - par['omega_stance']) / (1+np.exp(par['a']*y))            
+
+        omega = par['omega_stance'] + (par['omega_swing'] - par['omega_stance']) / (1+np.exp(par['a']*y))
         R = par['alpha']*(par['mu'] - x**2 - y**2)
-            
+
         return (R*x - omega*y).tolist() + (R*y + omega*x + par['K'].dot(y)).tolist()
 
     def jac(t, X):
         x = np.array(X[:6])
         y = np.array(X[6:])
-            
-        omega = par['omega_stance'] + (par['omega_swing'] - par['omega_stance']) / (1+np.exp(par['a']*y))            
+
+        omega = par['omega_stance'] + (par['omega_swing'] - par['omega_stance']) / (1+np.exp(par['a']*y))
         R = par['alpha']*(par['mu'] - x**2 - y**2)
-        
+
         dX = np.zeros([12,12])
         dX[6:11,6:11] = par['K']
         for i in range(6):
-            dX[i,i]     = - 2*par['alpha']*x[i]**2 + R[i] 
-            dX[i,i+6]   = - 2*par['alpha']*x[i]*y[i] - omega[i] + (omega[i]-par['omega_stance'])**2*par['a']*np.exp(par['a']*y[i])/(par['omega_swing']-par['omega_stance'])*y[i] 
+            dX[i,i]     = - 2*par['alpha']*x[i]**2 + R[i]
+            dX[i,i+6]   = - 2*par['alpha']*x[i]*y[i] - omega[i] + (omega[i]-par['omega_stance'])**2*par['a']*np.exp(par['a']*y[i])/(par['omega_swing']-par['omega_stance'])*y[i]
             dX[i+6,i]   = - 2*par['alpha']*x[i]*y[i] + omega[i]
             dX[i+6,i+6] = - 2*par['alpha']*y[i]*2 + R[i] - (omega[i]-par['omega_stance'])**2*par['a']*np.exp(par['a']*y[i])/(par['omega_swing']-par['omega_stance'])*x[i]
-            
+
         return dX.tolist()
 
     return f, jac
@@ -467,7 +465,7 @@ def fun_righetti_ijspeert(par = {'a', 'alpha', 'mu', 'K', 'omega_swing', 'omega_
 # Hysteresis
 # =============================================================================
 def fun_cdc2_cyclin(par = {'a1':1,'a2':1,'b1':200,'b2':10,'K1':30,'K2':1,'gamma1':4,'gamma2':4,'nu':1}):
-    """Cdc2-Cyclin B/Wee1 System. Exhibits hysteresis y1 steady, when varying 
+    """Cdc2-Cyclin B/Wee1 System. Exhibits hysteresis y1 steady, when varying
     \nu between 0 and 2 and back"""
     def f(t, X):
         x1, x2, y1, y2 = X
@@ -475,20 +473,20 @@ def fun_cdc2_cyclin(par = {'a1':1,'a2':1,'b1':200,'b2':10,'K1':30,'K2':1,'gamma1
         f2 = -par['a1']*x2 + (par['b1']*x1*(par['nu']*y1)**par['gamma1'])/(par['K1']+(par['nu']*y1)**par['gamma1'])
         f3 =  par['a2']*y2 - (par['b2']*y1*x1**par['gamma2'])/(par['K2']+x1**par['gamma2'])
         f4 = -par['a2']*y2 + (par['b2']*y1*x1**par['gamma2'])/(par['K2']+x1**par['gamma2'])
-        
+
         return [f1, f2, f3, f4]
 
     return f, None
 
 
 def fun_cdc2_cyclin_reduced(par = {'a1':1,'a2':1,'b1':200,'b2':10,'K1':30,'K2':1,'gamma1':4,'gamma2':4,'nu':1}):
-    """Cdc2-Cyclin B/Wee1 System. Exhibits hysteresis y1 steady, when varying 
+    """Cdc2-Cyclin B/Wee1 System. Exhibits hysteresis y1 steady, when varying
     \nu between 0 and 2 and back"""
     def f(t, X):
         x1, y1 = X
         f1 =  par['a1']*(1-x1) - (par['b1']*x1*(par['nu']*y1)**par['gamma1'])/(par['K1']+(par['nu']*y1)**par['gamma1'])
         f2 =  par['a2']*(1-y1) - (par['b2']*y1*x1**par['gamma2'])/(par['K2']+x1**par['gamma2'])
-        
+
         return [f1, f2]
 
     return f, None
--- DE_library/plotting.py
+++ DE_library/plotting.py
@@ -12,12 +12,13 @@ from mpl_toolkits.mplot3d import proj3d
 
 import numpy as np
 
-def time_series(T, 
-                X, 
-                style='o', 
-                node_feature=None, 
-                figsize=(10,5), 
-                lw=1, 
+
+def time_series(T,
+                X,
+                style='o',
+                node_feature=None,
+                figsize=(10, 5),
+                lw=1,
                 ms=5,
                 save=None):
     """
@@ -41,56 +42,58 @@ def time_series(T,
     ax : matplotlib axes object.
 
     """
-            
+
     if not isinstance(X, list):
         X = [X]
-            
-    fig = plt.figure(figsize=figsize, constrained_layout=True)  
+
+    fig = plt.figure(figsize=figsize, constrained_layout=True)
     grid = gridspec.GridSpec(len(X), 1, wspace=0.5, hspace=0, figure=fig)
-    
+
     for sp, X_ in enumerate(X):
-        
+
         if sp == 0:
             ax = plt.Subplot(fig, grid[sp])
         else:
             ax = plt.Subplot(fig, grid[sp], sharex=ax)
-            
+
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
-            
+
         if sp < len(X)-1:
             plt.setp(ax.get_xticklabels(), visible=False)
             ax.spines['bottom'].set_visible(False)
-            ax.xaxis.set_ticks_position('none') 
-        
+            ax.xaxis.set_ticks_position('none')
+
         colors = set_colors(node_feature)[0]
-                
+
         for i in range(len(X_)-2):
             if X_[i] is None:
                 continue
-            
-            c = colors[i] if len(colors)>1 and not isinstance(colors,str) else colors
-                    
-            ax.plot(T[i:i+2], X_[i:i+2], style, c=c, linewidth=lw, markersize=ms)
-            
+
+            c = colors[i] if len(colors) > 1 and not isinstance(
+                colors, str) else colors
+
+            ax.plot(T[i:i+2], X_[i:i+2], style, c=c,
+                    linewidth=lw, markersize=ms)
+
             fig.add_subplot(ax)
-        
+
     if save is not None:
         savefig(fig, save)
-        
+
     return ax
 
 
 def trajectories(X,
                  V,
-                 ax=None, 
-                 style='o', 
-                 node_feature=None, 
-                 lw=1, 
-                 ms=5, 
-                 arrowhead=1, 
+                 ax=None,
+                 style='o',
+                 node_feature=None,
+                 lw=1,
+                 ms=5,
+                 arrowhead=1,
                  arrow_spacing=3,
-                 axis=False, 
+                 axis=False,
                  alpha=None):
     """
     Plot trajectory in phase space. If multiple trajectories
@@ -116,152 +119,155 @@ def trajectories(X,
     ax : matplotlib axes object.
 
     """
-            
+
     dim = X.shape[1]
-    assert dim==2 or dim==3, 'Dimension must be 2 or 3.'
-    
+    assert dim == 2 or dim == 3, 'Dimension must be 2 or 3.'
+
     if ax is None:
         _, ax = create_axis(dim)
-            
+
     c = set_colors(node_feature)[0]
     if alpha is not None:
-        al=np.ones(len(X))*alpha
-    elif len(c)>1 and not isinstance(c, str):
-        al=np.abs(node_feature)/np.max(np.abs(node_feature))
+        al = np.ones(len(X))*alpha
+    elif len(c) > 1 and not isinstance(c, str):
+        al = np.abs(node_feature)/np.max(np.abs(node_feature))
     else:
-        al=1
-                
-    if dim==2:
+        al = 1
+
+    if dim == 2:
         if 'o' in style:
-            ax.scatter(X[:,0], X[:,1], c=c, s=ms, alpha=al)
+            ax.scatter(X[:, 0], X[:, 1], c=c, s=ms, alpha=al)
         if '-' in style:
-            ax.plot(X[:,0], X[:,1], c=c, linewidth=lw, markersize=ms, alpha=al)
+            ax.plot(X[:, 0], X[:, 1], c=c, linewidth=lw,
+                    markersize=ms, alpha=al)
         if '>' in style:
             arrow_prop_dict = dict(color=c, alpha=al, lw=lw)
             skip = (slice(None, None, arrow_spacing), slice(None))
             X, V = X[skip], V[skip]
             for j in range(X.shape[0]):
-                ax.quiver(X[j,0], X[j,1], V[j,0]*0.1, V[j,1]*0.1,
+                ax.quiver(X[j, 0], X[j, 1], V[j, 0]*0.1, V[j, 1]*0.1,
                           **arrow_prop_dict)
-    elif dim==3:
+    elif dim == 3:
         if 'o' in style:
-            ax.scatter(X[:,0], X[:,1], X[:,2], c=c, s=ms, alpha=al)
+            ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=c, s=ms, alpha=al)
         if '-' in style:
-            ax.plot(X[:,0], X[:,1], X[:,2], c=c, linewidth=lw, markersize=ms, alpha=al)
+            ax.plot(X[:, 0], X[:, 1], X[:, 2], c=c,
+                    linewidth=lw, markersize=ms, alpha=al)
         if '>' in style:
-            arrow_prop_dict = dict(mutation_scale=arrowhead, arrowstyle='-|>', color=c, alpha=al, lw=lw)
+            arrow_prop_dict = dict(
+                mutation_scale=arrowhead, arrowstyle='-|>', color=c, alpha=al, lw=lw)
             skip = (slice(None, None, arrow_spacing), slice(None))
             X, V = X[skip], V[skip]
             for j in range(X.shape[0]):
-                a = Arrow3D([X[j,0], X[j,0]+V[j,0]], 
-                            [X[j,1], X[j,1]+V[j,1]], 
-                            [X[j,2], X[j,2]+V[j,2]], 
+                a = Arrow3D([X[j, 0], X[j, 0]+V[j, 0]],
+                            [X[j, 1], X[j, 1]+V[j, 1]],
+                            [X[j, 2], X[j, 2]+V[j, 2]],
                             **arrow_prop_dict)
                 ax.add_artist(a)
-                
+
     if not axis:
         ax = set_axes(ax, off=True)
-        
+
     return ax
 
 
 class Arrow3D(FancyArrowPatch):
     def __init__(self, xs, ys, zs, *args, **kwargs):
-        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        FancyArrowPatch.__init__(self, (0, 0), (0, 0), *args, **kwargs)
         self._verts3d = xs, ys, zs
 
     def draw(self, renderer):
         xs3d, ys3d, zs3d = self._verts3d
         xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, self.axes.M)
-        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
         FancyArrowPatch.draw(self, renderer)
-        
+
     def do_3d_projection(self, renderer=None):
         xs3d, ys3d, zs3d = self._verts3d
         xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, self.axes.M)
-        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
 
         return np.min(zs)
 
-        
 
 # =============================================================================
 # Helper functions
 # =============================================================================
 def create_axis(*args, fig=None):
-    
+
     dim = args[0]
-    if len(args)>1:
-        args = [args[i] for i in range(1,len(args))]
+    if len(args) > 1:
+        args = [args[i] for i in range(1, len(args))]
     else:
-        args = (1,1,1)
-    
+        args = (1, 1, 1)
+
     if fig is None:
         fig = plt.figure()
-        
-    if dim==2:
+
+    if dim == 2:
         ax = fig.add_subplot(*args)
-    elif dim==3:
+    elif dim == 3:
         ax = fig.add_subplot(*args, projection="3d")
-        
+
     return fig, ax
 
 
 def get_limits(ax):
     lims = [ax.get_xlim(), ax.get_ylim()]
-    if ax.name=="3d":
+    if ax.name == "3d":
         lims.append(ax.get_zlim())
-        
+
     return lims
 
 
 def set_axes(ax, lims=None, padding=0.1, off=True):
-    
+
     if lims is not None:
         xlim = lims[0]
         ylim = lims[1]
         pad = padding*(xlim[1] - xlim[0])
-        
+
         ax.set_xlim([xlim[0]-pad, xlim[1]+pad])
         ax.set_ylim([ylim[0]-pad, ylim[1]+pad])
-        if ax.name=="3d":
+        if ax.name == "3d":
             zlim = lims[2]
             ax.set_zlim([zlim[0]-pad, zlim[1]+pad])
-        
+
     if off:
         ax.set_yticklabels([])
         ax.set_xticklabels([])
-        if ax.name=="3d":
-            ax.set_zticklabels([])        
-    
+        if ax.name == "3d":
+            ax.set_zticklabels([])
+
     return ax
 
 
 def set_colors(color, cmap=plt.cm.coolwarm):
-    
+
     if color is None:
         return 'C0', None
     else:
         assert isinstance(color, (list, tuple, np.ndarray))
-        
+
     if isinstance(color[0], (float, np.floating)):
-        if (color>=0).all():
-            
+        if (color >= 0).all():
+
             norm = plt.cm.colors.Normalize(0, np.max(np.abs(color)))
-        else:    
-            norm = plt.cm.colors.Normalize(-np.max(np.abs(color)), np.max(np.abs(color)))
-        
+        else:
+            norm = plt.cm.colors.Normalize(-np.max(np.abs(color)),
+                                           np.max(np.abs(color)))
+
         colors = []
         for i, c in enumerate(color):
             colors.append(cmap(norm(np.array(c).flatten())))
-   
+
     elif isinstance(color[0], int):
         colors = [f"C{i}" for i in np.arange(1, color.max()+1)]
-        cmap, norm = matplotlib.colors.from_levels_and_colors(np.arange(1, color.max()+2), 
+        cmap, norm = matplotlib.colors.from_levels_and_colors(np.arange(1, color.max()+2),
                                                               colors)
-        
+
     cbar = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
-            
+
     return colors, cbar
 
 
